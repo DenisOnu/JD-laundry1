0211b9cdb87ab4843c9cb4db37961170e0f742e3
Denis <denisonu7@gmail.com>
2022-11-23 13:35:47 +0100

    next commit

--- config.php
+++ config.php
@@ -1,25 +1,25 @@
 <?php
     session_start();
-    //connect to database
-    $conn = mysqli_connect("localhost", "root", "", "laundrydb");
+    // //connect to database
+    // $conn = mysqli_connect("localhost", "root", "", "laundrydb");
 
-    if (!$conn) {
-        die("Error connecting to database: " . mysqli_connect_error());
-    }
-    // define global constants
-    define ('ROOT_PATH', realpath(dirname(__FILE__)));
-    define('BASE_URL', 'http://localhost/jdlaundry/');
+    // if (!$conn) {
+    //     die("Error connecting to database: " . mysqli_connect_error());
+    // }
+    // // define global constants
+    // define ('ROOT_PATH', realpath(dirname(__FILE__)));
+    // define('BASE_URL', 'http://localhost/jdlaundry/');
 
     
-    // //Get Heroku ClearDB connection information
-    // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    // $cleardb_server = $cleardb_url["host"];
-    // $cleardb_username = $cleardb_url["user"];
-    // $cleardb_password = $cleardb_url["pass"];
-    // $cleardb_db = substr($cleardb_url["path"],1);
-    // $active_group = 'default';
-    // $query_builder = TRUE;
-    // // Connect to DB
-    // $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
     
 ?>
